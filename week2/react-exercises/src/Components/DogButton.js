bc2ab49430945495508f60c4c7a5a4cbee4f645f
mucahit04 <57506115+mucahit04@users.noreply.github.com>
2020-06-16 19:17:56 +0200

    updated regarding the changes on dogGallery component

--- week2/react-exercises/src/Components/DogButton.js
+++ week2/react-exercises/src/Components/DogButton.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function DogButton({ getDogPhoto }) {
+export default function DogButton({ setClick, click }) {
 	return (
-		<button className='button' onClick={() => getDogPhoto()}>
+		<button className='button' onClick={() => setClick(click + 1)}>
 			Get a dog!
 		</button>
 	);
