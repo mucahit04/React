f29580794164c3543213180abb9c8695b1947e40
mucahit04 <57506115+mucahit04@users.noreply.github.com>
2020-06-16 17:37:34 +0200

    used status state to get away handling the error and loading status

--- week2/project/hackyourweather/src/components/Search.js
+++ week2/project/hackyourweather/src/components/Search.js
@@ -1,58 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import CityItem from "./CityItem";
-import { useState } from "react";
 
 export default function Search() {
 	const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
 
-	const [error, setError] = useState(false);
-	const [isLoading, setLoading] = useState(false);
 	const [cityName, setCityName] = useState("");
 	const [cityData, setCityData] = useState({});
+	const [status, setStatus] = useState();
 
 	const fetchCityWeather = async () => {
-		setLoading(true);
+		setStatus("loading");
 		try {
-			const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+			const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
 			const res = await fetch(apiUrl);
 			if (!res.ok) {
-				setError(true);
-				setLoading(false);
+				setStatus("error");
 			} else {
+				setStatus("success");
 				const data = await res.json();
 				setCityData(data);
-				setLoading(false);
-				setError(false);
 			}
 		} catch (error) {
-			setError(true);
+			setStatus("error");
 		}
-	};
-	const handleSubmit = e => {
-		e.preventDefault();
-		fetchCityWeather();
 	};
 
 	return (
 		<>
 			<div className='search-div'>
 				{!cityData.name && <p>Type a city name and search for the weather</p>}
-				{isLoading && <p>Loading..</p>}
-				<form onSubmit={handleSubmit} className='form'>
-					<input
-						type='text'
-						className='input'
-						value={cityName}
-						onChange={event => {
-							let value = event.target.value;
-							setCityName(value);
-						}}
-					/>
-					<input className='button' type='submit' value='Search' />
-				</form>
+				{status === "loading" && <p>Loading..</p>}
+				{status === "error" && <p>Fetch error!</p>}
 
-				{error && !isLoading && <p>Fetch error!</p>}
-				{!isLoading && !error && cityData.name && (
+				<input
+					className='input'
+					type='text'
+					value={cityName}
+					onChange={event => {
+						const value = event.target.value;
+						setCityName(value);
+					}}
+				/>
+				<button onClick={() => fetchCityWeather()}>Search</button>
+
+				{status === "success" && cityData.name && (
 					<div className='city-item'>
 						<CityItem cityData={cityData} />
 					</div>
