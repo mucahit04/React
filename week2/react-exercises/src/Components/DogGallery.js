cc10a9f33232cb7ca208aff18ebaccea76fc8476
mucahit04 <57506115+mucahit04@users.noreply.github.com>
2020-06-16 19:17:12 +0200

    added useEffect feature to fetch data

--- week2/react-exercises/src/Components/DogGallery.js
+++ week2/react-exercises/src/Components/DogGallery.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DogButton from "./DogButton";
-import { useState } from "react";
 import DogPhoto from "./DogPhoto";
-// import DogPhoto from "./DogPhoto";
 
 export default function DogGallery() {
 	const [dogPhotos, setDogPhotos] = useState([]);
 	const [hasError, setError] = useState(false);
 	const [isLoading, setLoading] = useState(false);
+	const [click, setClick] = useState(0);
 
-	function getDogPhoto() {
-		setLoading(true);
-		fetch("https://dog.ceo/api/breeds/image/random")
-			.then(res => {
-				if (res.status === 200) {
-					setLoading(false);
-					return res.json();
-				} else {
-					setError(true);
-				}
-			})
-			.then(data => setDogPhotos(dogPhotos => [...dogPhotos, data.message]));
-	}
+	useEffect(() => {
+		function getDogPhoto() {
+			if (click !== 0) {
+				setLoading(true);
+				fetch("https://dog.ceo/api/breeds/image/random")
+					.then(res => {
+						if (res.status === 200) {
+							setLoading(false);
+							return res.json();
+						} else {
+							setError(true);
+						}
+					})
+					.then(data => setDogPhotos(dogPhotos => [...dogPhotos, data.message]));
+			}
+		}
+		getDogPhoto();
+	}, [click]);
 	return (
 		<>
 			<div className='dogButton-div'>
-				<DogButton getDogPhoto={getDogPhoto} />
+				<DogButton setClick={setClick} click={click} />
 			</div>
 			<div>
 				{hasError && <p>Ooops! Thats an error..</p>}
